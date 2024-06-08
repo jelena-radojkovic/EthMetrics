7e496537fb284109d38139d0a8ee5b7d90cb738b
Jelena Radojkovic <jradojkovic22@gmail.com>
2024-06-08 19:06:05 +0200

    Initial Commit

---
+++ MetricsWidget/MetricsWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetricsWidgetApp.swift
+//  MetricsWidget
+//
+//  Created by Macbook Pro  on 8.6.24..
+//
+
+import SwiftUI
+
+@main
+struct MetricsWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
