6be28dbed4b24dafda1eb719ad1a473aa264a923
Jelena Radojkovic <jradojkovic22@gmail.com>
2024-06-30 16:08:52 +0200

    Added mocked data

--- MetricsWidget/Home/HomeView.swift
+++ MetricsWidget/Home/HomeView.swift
@@ -11,159 +11,47 @@ import Charts
 struct HomeView: View {
     @ObservedObject var viewModel = HomeViewModel()
     
-    let mockData: [Point] = [
-        Point(id: 0, x: 1, y: 0),
-        Point(id: 1, x: 2, y: 2),
-        Point(id: 2, x: 3, y: 4),
-        Point(id: 3, x: 4, y: 5),
-        Point(id: 4, x: 5, y: 8),
-        Point(id: 5, x: 6, y: 12)
-    ]
+    enum Constants {
+        static let titleColor = 0x0C1844
+        static let cardColor = 0xF9F9E0
+        static let backgroundColor = 0x028391
+    }
+    
     var body: some View {
         ZStack {
             ScrollView {
                 Text("Choose metrics you want to display in widget")
-                    .foregroundColor(Color(hex: 0x0C1844, opacity: 1))
+                    .foregroundColor(Color(hex: Constants.titleColor, opacity: 1))
                     .padding(.all, 30)
                     .multilineTextAlignment(.center).fontWeight(.bold)
                 
-                HStack {
-                    Text("Gas used")
-                        .padding(.leading, 20)
+                ForEach(Array(viewModel.streamingValues.keys), id: \.self) { key in
                     
-                    Spacer()
+                    HStack {
+                        Text(key)
+                            .padding(.leading, 20)
+                        
+                        Spacer()
+                        
+                        if let chartData = viewModel.streamingValues[key] {
+                            Chart(chartData) {
+                                LineMark(
+                                    x: .value("x", $0.x),
+                                    y: .value("y", $0.y)
+                                )
+                            }.padding([.top, .bottom], 20)
+                                .padding(.trailing, 20)
+                                .frame(width: 150)
+                        }
+                    }
+                    .frame(height: 130)
+                    .background(Color(hex: Constants.cardColor, opacity: 1))
+                    .cornerRadius(16)
                     
-                    Chart(mockData) {
-                        LineMark(
-                            x: .value("x", $0.x),
-                            y: .value("y", $0.y)
-                        )
-                    }.padding([.top, .bottom], 20)
-                        .padding(.trailing, 20)
-                        .frame(width: 150)
                 }
-                .frame(height: 130)
-                .background(Color(hex: 0xF9F9E0, opacity: 1))
-                .cornerRadius(16)
-            
-                
-                HStack {
-                    Text("Gas limit")
-                        .padding(.leading, 20)
-                    
-                    Spacer()
-                    
-                    Chart(mockData) {
-                        LineMark(
-                            x: .value("x", $0.x),
-                            y: .value("y", $0.y)
-                        )
-                    }.padding([.top, .bottom], 20)
-                        .padding(.trailing, 20)
-                        .frame(width: 150)
-                }
-                .frame(height: 130)
-                .background(Color(hex: 0xF9F9E0, opacity: 1))
-                .cornerRadius(16)
-                
-                HStack {
-                    Text("Gas price")
-                        .padding(.leading, 20)
-                    
-                    Spacer()
-                    
-                    Chart(mockData) {
-                        LineMark(
-                            x: .value("x", $0.x),
-                            y: .value("y", $0.y)
-                        )
-                    }.padding([.top, .bottom], 20)
-                        .padding(.trailing, 20)
-                        .frame(width: 150)
-                }
-                .frame(height: 130)
-                .background(Color(hex: 0xF9F9E0, opacity: 1))
-                .cornerRadius(16)
-                
-                HStack {
-                    Text("Value")
-                        .padding(.leading, 20)
-                    
-                    Spacer()
-                    
-                    Chart(mockData) {
-                        LineMark(
-                            x: .value("x", $0.x),
-                            y: .value("y", $0.y)
-                        )
-                    }.padding([.top, .bottom], 20)
-                        .padding(.trailing, 20)
-                        .frame(width: 150)
-                }
-                .frame(height: 130)
-                .background(Color(hex: 0xF9F9E0, opacity: 1))
-                .cornerRadius(16)
-                
-                HStack {
-                    Text("Gas")
-                        .padding(.leading, 20)
-                    
-                    Spacer()
-                    
-                    Chart(mockData) {
-                        LineMark(
-                            x: .value("x", $0.x),
-                            y: .value("y", $0.y)
-                        )
-                    }.padding([.top, .bottom], 20)
-                        .padding(.trailing, 20)
-                        .frame(width: 150)
-                }
-                .frame(height: 130)
-                .background(Color(hex: 0xF9F9E0, opacity: 1))
-                .cornerRadius(16)
-                
-                HStack {
-                    Text("Max priority fee per gas")
-                        .padding(.leading, 20)
-                    
-                    Spacer()
-                    
-                    Chart(mockData) {
-                        LineMark(
-                            x: .value("x", $0.x),
-                            y: .value("y", $0.y)
-                        )
-                    }.padding([.top, .bottom], 20)
-                        .padding(.trailing, 20)
-                        .frame(width: 150)
-                }
-                .frame(height: 130)
-                .background(Color(hex: 0xF9F9E0, opacity: 1))
-                .cornerRadius(16)
-                
-                HStack {
-                    Text("Base fee per gas")
-                        .padding(.leading, 20)
-                    
-                    Spacer()
-                    
-                    Chart(mockData) {
-                        LineMark(
-                            x: .value("x", $0.x),
-                            y: .value("y", $0.y)
-                        )
-                    }.padding([.top, .bottom], 20)
-                        .padding(.trailing, 20)
-                        .frame(width: 150)
-                }
-                .frame(height: 130)
-                .background(Color(hex: 0xF9F9E0, opacity: 1))
-                .cornerRadius(16)
-                
             }.padding(.horizontal, 30)
         }
-        .background(Color(hex: 0x028391, opacity: 1))
+        .background(Color(hex: Constants.backgroundColor, opacity: 1))
     }
 }
 
--- MetricsWidget/Home/HomeViewModel.swift
+++ MetricsWidget/Home/HomeViewModel.swift
@@ -10,22 +10,60 @@ import WidgetKit
 
 
 struct Point: Identifiable {
-    var id: Int
+    let id = UUID()
     let x: Int
     let y: Int
 }
 
 class HomeViewModel: ObservableObject {
-    let appGroupIdentifier = "group.com.ethmetrics"
+    let appGroupIdentifier = Constants.appGroupId
+    @Published var streamingValues: [String: [Point]] = [:]
     
-    // niz od sedam polja, svako polje ima niz poslednjih 10 vrednosti
-    // tih 10 vrednosti treba da budu queue
-    @Published var streamingValues: [String: [Int]] = [:]
+    enum Constants {
+        static let appGroupId = "group.com.ethmetrics"
+        static let cacheKey = "sharedData"
+        static let gasUsed = "Gas used"
+        static let gasLimit = "Gas limit"
+        static let gasPrice = "Gas price"
+        static let value = "Value"
+        static let gas = "Gas"
+        static let maxPriority = "Max priority fee per gas"
+        static let baseFee = "Base fee per gas"
+    }
+    
+    init() {
+        streamingValues[Constants.gasUsed] = []
+        streamingValues[Constants.gasPrice] = []
+        streamingValues[Constants.value] = []
+        streamingValues[Constants.gas] = []
+        streamingValues[Constants.gasLimit] = []
+        streamingValues[Constants.maxPriority] = []
+        streamingValues[Constants.baseFee] = []
+        
+        fetchMockData()
+    }
+    
+    func fetchMockData() {
+        DispatchQueue.global(qos: .userInteractive).async {
+            while true {
+                Thread.sleep(forTimeInterval: 2)
+                
+                for key in self.streamingValues.keys {
+                    let randomY = Int.random(in: 0..<10)
+                    
+                    DispatchQueue.main.async {
+                        let x = self.streamingValues[key]!.last?.x ?? 0
+                        self.streamingValues[key]!.append(Point(x: x + 1, y: randomY))
+                    }
+                }
+            }
+        }
+    }
     
     func updateWidgetData(newData: String) {
         // Save data to UserDefaults in the shared app group
         if let userDefaults = UserDefaults(suiteName: appGroupIdentifier) {
-            userDefaults.setValue(newData, forKey: "sharedData")
+            userDefaults.setValue(newData, forKey: Constants.cacheKey)
         }
         
         // Trigger widget update
